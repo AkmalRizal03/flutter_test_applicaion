690e284bee11e844891f6bbf104fed2c9ec541e9
Akmal Rizal <akmal.rizal22@gmail.com>
2024-03-19 21:06:50 +0800

    Initial commit

---
+++ lib/counter_model.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CounterModel extends ChangeNotifier {
+  int _count = 0;
+
+  int get count => _count;
+
+  void increment() {
+    _count++;
+    notifyListeners(); // Notify listeners about the change
+  }
+}
